35bdb9286f94fdba8eca4939c15d34fa54bf96d3
Burak Sakallı <buraksakalli06@gmail.com>
2019-12-24 11:10:07 +0300

    screenshots added.

--- js/main.js
+++ js/main.js
@@ -1,51 +1,64 @@
 let button = document.querySelector('.wrapper');
-let arrayLength = 16, x = 1;
-let cards = ["Karo2", "Karo2", "Maca2", "Maca2", "Sinek2", "Sinek2", "KaroA", "KaroA", "MacaA", "MacaA", "SinekA", "SinekA", "KupaA", "KupaA", "Kupa2", "Kupa2"];
-let n1, n2, turn = 0;
+let moveElement = document.querySelector('.move');
+let restartElement = document.querySelector('.restart');
 let cardElement = document.querySelector('#c');
+
+let move = 0; // total move
+let arrayLength = 16, x = 1; // arrayLength and loop value
+let cards = []; // Array of cards
+let m1, m2, turn = 0; // value of cards and turn
 let div1, div2;
 let old1, old2;
 let started = true;
+let totalRight = 0;
 
 button.addEventListener('click', (e) => {
-  let selected = e.target.getAttribute('data-key'); // could be null!
   let element = e.target.getAttribute('data-key');
   let id = document.querySelector("#c" + element);
-  console.log(id);
-  console.log(selected);
-  if (id.id != "ok" && started) {
-    turn += 1;
-    if (turn == 1) {
-      n1 = document.querySelector('#i' + element).src;
-      old1 = element;
-      div1 = document.querySelector('#i' + element);
-      console.log(n1);
-      div1.id = "ok";
-      div1.style.visibility = 'visible';
-    }
 
-    if (turn == 2) {
-      n2 = document.querySelector('#i' + element).src;
-      old2 = element;
-      div2 = document.querySelector('#i' + element);
-      turn = 0;
-      div2.id = "ok";
-      div2.style.visibility = 'visible';
-      started = false;
-      found();
+  if (started) {
+    if (id.id != "ok") {
+      turn += 1;
+      if (turn == 1) {
+        m1 = document.querySelector('#i' + element).src;
+        old1 = element;
+        div1 = document.querySelector('#i' + element);
+        div1.id = "ok";
+        div1.style.visibility = 'visible';
+      }
+
+      if (turn == 2) {
+        m2 = document.querySelector('#i' + element).src;
+        old2 = element;
+        div2 = document.querySelector('#i' + element);
+        turn = 0;
+        div2.id = "ok";
+        div2.style.visibility = 'visible';
+        started = false;
+        check();
+      }
     }
   }
 });
 
-found = () => {
-  if (n1 == n2) {
+check = () => {
+  if (m1 == m2) {
     div1.style.visibility = 'visible';
     div2.style.visibility = 'visible';
-    console.log("buldun");
+
+    totalRight++;
+    if (totalRight == 8) {
+      moveElement.innerText = "Game finished. Your score is " + move;
+      restartElement.style.display = 'block';
+    }
     started = true;
-  } else {
+  }
+  else {
     div1.id = "i" + old1;
     div2.id = "i" + old2;
+    move++;
+    moveElement.innerText = move;
+
     setTimeout(() => {
       div1.style.visibility = 'hidden';
       div2.style.visibility = 'hidden';
@@ -55,24 +68,32 @@ found = () => {
 }
 
 initial = () => {
+  cards = ["Karo2", "Karo2", "Maca2", "Maca2", "Sinek2", "Sinek2", "KaroA", "KaroA", "MacaA", "MacaA", "SinekA", "SinekA", "KupaA", "KupaA", "Kupa2", "Kupa2"];
   while (true) {
     let random = Math.floor(Math.random() * arrayLength);
-    cards = cards.filter((e) => {
+
+    cards = cards.filter((e) => { // Removing undefined objects from array.
       return e !== undefined;
     });
-    let card = cards[random];
 
-    if (card !== undefined) {
+    let card = cards[random]; // Getting a card.
+
+    if (card !== undefined) { // Random can get undefined object
       let define = document.querySelector('#c' + x);
-      define.innerText = card;
       define.innerHTML = '<img id="i' + x + '" class="img-card" src="src/' + card + '.png" data-key="' + x + '">';
-      delete cards[random];
-      arrayLength -= 1;
-      x++;
+
+      delete cards[random]; // Delete selected object
+      arrayLength -= 1; // Array decrease 1
+      x++; // Indis increase 1
     }
 
-    if (arrayLength == 0) return true;
+    if (arrayLength == 0) return true; // All objects assigned to elements
   }
 }
 
+restartElement.addEventListener('click', () => {
+  location.reload();
+});
+
+moveElement.innerText = move;
 initial();
